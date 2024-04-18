37e76ab398138c486d66176f74d12043c6d15ec1
Rahul Kumar mali <158578518+rahulkumarmali@users.noreply.github.com>
2024-04-19 00:01:07 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,113 @@
+
+<html>
+<head>
+<title>
+</title>
+<style>
+  .navbar{
+          margin-top: 1px;
+          background-color:#1b8989;
+          padding: 10px;
+         margin-left: -8px;
+    margin-right: -8px;
+       }
+  .navbar a{
+              text-decoration: none;
+              font-family: cursive;
+              color: white;
+              margin-left: 9px;
+             }
+    
+  .bg{
+    background-repeat: no-repeat;
+    background-size: 100%;
+    width: 1266px;
+    height: 422px;
+    margin-top: 0px;
+    margin-left: -11px;
+      }
+      .float-right{
+        float:right;
+
+      }
+      .logo{
+        width: 167px;
+    height: 144px;
+    margin-top: -8px;
+    margin-left: -11px;
+}
+.cc{
+     margin-left:30px;
+     margin-right:65px;
+     color:black;
+    }
+.nn{
+     color:green;
+     font-size:22px;
+    }
+</style>
+</head>
+<body class="body container-fluid">
+<img class="logo" src="image/log.jpg"></img>
+
+  <nav class="navbar">
+    <a class="ss" href="#">Home</a>
+    <a class="ss" href="about.php">About</a> 
+    <a class="ss" href="gallery.php">Gallery</a> 
+    <a class="ss" href="contact.php">Contact</a>
+    <!-- <a class="ss" href="buy.php">Buy Product</a> -->
+<div class=" float-right">
+  <a  class="btn" href="login.php">Login</a>
+  <a class="btn" href="register.php">Register</a>
+
+
+</div>
+
+
+  </nav>
+ <img class="bg" src="image\back.jpg"></img>
+      <h1  class="nn"align="center">NETURAL PRODUCT</h1>
+      <p class="cc"align="justify">
+        Natural products-Natural products are small molecules produced by
+        living organisms including plants,
+        invertebrates and microorganisms. They are also known as secondary
+        metabolites being non-essential for life;
+        but they play key roles in defence and cell to cell communication.
+        </br>
+        A natural product is a natural compound or substance produced by 
+        a living organism—that is, found in nature. In the broadest sense,
+        natural products include any substance produced by life.Natural 
+        products can also be prepared by chemical synthesis (both semisynthesis
+        and total synthesis) and have played a central role in the development
+        of the field of organic chemistry by providing challenging synthetic 
+        targets. The term natural product has also been extended for commercial 
+        purposes to refer to cosmetics, dietary supplements, and foods produced
+        from natural sources without added artificial ingredients.
+        Within the field of organic chemistry, 
+        the definition of natural products is usually restricted to organic compounds 
+        isolated from natural sources that are produced by the pathways of primary
+        or secondary metabolism.Within the field of medicinal chemistry,
+        the definition is often further restricted to secondary metabolites.
+        Secondary metabolites (or specialized metabolites) are not essential for survival,
+        but nevertheless provide organisms that produce them an evolutionary advantage.
+        Many secondary metabolites are cytotoxic and have been selected and 
+        optimized through evolution for use as "chemical warfare" agents against prey,
+       predators, and competing organisms.Secondary or specialized metabolites
+       are often unique to species, which is contrasted to primary metabolites which 
+       have broad use across kingdoms. Secondary metabolites are marked by chemical
+       complexity which is why they are of such interest to chemists.
+       Natural sources may lead to basic research on potential bioactive
+       components for commercial development as lead compounds in drug discovery.
+      Although natural products have inspired numerous drugs, 
+       drug development from natural sources has received declining attention in the
+       21st century by pharmaceutical companies, partly due to unreliable access and supply, 
+       intellectual property, cost, and profit concerns, seasonal or environmental variability 
+       of composition, and loss of sources due to rising extinction rates.
+      </p>
+      <p  class="df"align="justify">
+        The broadest definition of a natural product is
+        anything that is produced by life, and includes biotic 
+        materials (e.g. wood, silk), bio-based materials (e.g. bioplastics, cornstarch),
+      </p>
+</body>
+</html>
